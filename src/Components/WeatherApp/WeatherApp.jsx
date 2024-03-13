b0726d146e8042b8439a89a24423e68e0b2ab06c
alaaeddine <alaaeddine.ahriz@insa-lyon.fr>
2024-03-13 10:04:47 +0100

    Update Weather Icon Depending on API result

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 
 import search_icon from "../Assets/search.png";
@@ -18,6 +18,8 @@ export const WeatherApp = () => {
 
     let api_key = "41ad9850d25b95de0ec5b350ddd03b16";
 
+    const [wicon,setWicon] = useState(few_clouds);
+
     const search = async ( ) => {
         const element = document.getElementsByClassName("cityInput");
         if(element[0].value==="")
@@ -35,10 +37,55 @@ export const WeatherApp = () => {
         const temperature = document.getElementsByClassName("weather-temp");
         const location = document.getElementsByClassName("weather-location");
 
-        humidity[0].innerHTML = data.main.humidity;
-        wind[0].innerHTML = data.wind.speed;
-        temperature[0].innerHTML = data.main.temp;
+        humidity[0].innerHTML = data.main.humidity+" %";
+        wind[0].innerHTML = data.wind.speed+" km/h";
+        temperature[0].innerHTML = data.main.temp+"°C";
         location[0].innerHTML = data.name;
+
+        if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n")
+        {
+            setWicon(clear_sky);
+        }
+        
+        if(data.weather[0].icon==="02d" || data.weather[0].icon==="02n")
+        {
+            setWicon(few_clouds);
+        }
+
+        if(data.weather[0].icon==="03d" || data.weather[0].icon==="03n")
+        {
+            setWicon(scattered_clouds);
+        }
+
+        if(data.weather[0].icon==="04d" || data.weather[0].icon==="04n")
+        {
+            setWicon(broken_clouds);
+        }
+
+        if(data.weather[0].icon==="09d" || data.weather[0].icon==="09n")
+        {
+            setWicon(shower_rain);
+        }
+
+        if(data.weather[0].icon==="10d" || data.weather[0].icon==="10n")
+        {
+            setWicon(rain);
+        }
+
+        if(data.weather[0].icon==="11d" || data.weather[0].icon==="11n")
+        {
+            setWicon(thunderstorm);
+        }
+
+        if(data.weather[0].icon==="13d" || data.weather[0].icon==="13n")
+        {
+            setWicon(snow);
+        }
+
+        if(data.weather[0].icon==="50d" || data.weather[0].icon==="50n")
+        {
+            setWicon(mist);
+        }
     }
 
   return (
@@ -46,7 +93,7 @@ export const WeatherApp = () => {
         <div className="top-bar">
             <input type="text" className="cityInput" placeholder='Search'/>
             <div className="search-icon" onClick={()=>{search()}}>
-                <img src={search_icon} alt="" />
+                <img src={wicon} alt="" />
             </div>
         </div>
         <di className="weather-image">
