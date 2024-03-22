f5587e65a692ced4b64d33408d4a461f5441b112
alaaeddine <alaaeddine.ahriz@insa-lyon.fr>
2024-03-22 23:32:43 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { WeatherApp } from './Components/WeatherApp/WeatherApp';
 
+
 export default App;
 
 function App() {
   return (
-    <><div className="App">
-      <WeatherApp />
-    </div></>
+    <div className="App">
+      {<WeatherApp />}
+    </div>
   );
 }
