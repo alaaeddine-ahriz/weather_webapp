ada5bde99bf0e0a9ed4e04b9ca882378c4bf38f2
alaaeddine <alaaeddine.ahriz@insa-lyon.fr>
2024-03-26 21:55:04 +0100

    prettier

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import MapComponent from "./MapComponent";
 import { Header } from "./HeaderComponent";
-import './WeatherApp.css';
+import "./WeatherApp.css";
 
 // Importing images and icons
 // import search_icon from "../Assets/search.png";
@@ -24,121 +24,123 @@ import './WeatherApp.css';
 
 import clearSkyImage from "../Assets/clear_sky_img.jpg";
 
-
-
 export const center = {
-    lat: 7.2905715,
-    lng: 80.6337262,
+  lat: 7.2905715,
+  lng: 80.6337262,
 };
 
 export const WeatherApp = () => {
-    const [backgroundVideo, setBackgroundVideo] = useState(clearSkyImage);
-    const [center, setCenter] = useState({
-      lat: 7.2905715,
-      lng: 80.6337262,
-    });
-    const [weatherData, setWeatherData] = useState({
-      humidity: '-%',
-      windSpeed: '-km/h',
-      temp: '-°',
-      feelsLike: '-°',
-      location: 'Villeurbanne',
-    });
+  const [backgroundVideo, setBackgroundVideo] = useState(clearSkyImage);
+  const [center, setCenter] = useState({
+    lat: 7.2905715,
+    lng: 80.6337262,
+  });
+  const [weatherData, setWeatherData] = useState({
+    humidity: "-%",
+    windSpeed: "-km/h",
+    temp: "-°",
+    feelsLike: "-°",
+    location: "Villeurbanne",
+  });
 
-    const api_key = "41ad9850d25b95de0ec5b350ddd03b16";
-  
-    const fetchWeatherAndGeocodeData = async (city) => {
-      try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${api_key}`);
-        if (!response.ok) throw new Error("Weather data fetch failed");
-        let data = await response.json();
+  const api_key = "41ad9850d25b95de0ec5b350ddd03b16";
 
-        setWeatherData({
-          humidity: `${Math.floor(data.main.humidity)}%`,
-          windSpeed: `${Math.floor(data.wind.speed)} km/h`,
-          temp: `${Math.floor(data.main.temp)}°`,
-          feelsLike: `${Math.floor(data.main.feels_like)}°`,
-          location: data.name,
-        });
-        
-        const weatherVideoMap = {
-          "01d": clearSkyImage,
-          "02d": clearSkyImage,
-          "03d": clearSkyImage,
-          "04d": clearSkyImage,
-          "09d": clearSkyImage,
-          "10d": clearSkyImage,
-          "11d": clearSkyImage,
-          "13d": clearSkyImage,
-          "50d": clearSkyImage,
-        };
-        setBackgroundVideo(weatherVideoMap[data.weather[0].icon] || clearSkyImage);
+  const fetchWeatherAndGeocodeData = async (city) => {
+    try {
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${api_key}`,
+      );
+      if (!response.ok) throw new Error("Weather data fetch failed");
+      let data = await response.json();
 
-        const geoResponse = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyAW0-OQUNUuQHQ-TvSuo4v4GjRKmHE1eps`);
-        const geoData = await geoResponse.json();
-        
-        if (geoData.results && geoData.results.length > 0) {
-          const location = geoData.results[0].geometry.location;
-          setCenter({ lat: location.lat, lng: location.lng });
-        } else {
-          console.error('No results found for the specified city.');
-        }
-      } catch (error) {
-        console.error("Failed to fetch weather data:", error);
+      setWeatherData({
+        humidity: `${Math.floor(data.main.humidity)}%`,
+        windSpeed: `${Math.floor(data.wind.speed)} km/h`,
+        temp: `${Math.floor(data.main.temp)}°`,
+        feelsLike: `${Math.floor(data.main.feels_like)}°`,
+        location: data.name,
+      });
+
+      const weatherVideoMap = {
+        "01d": clearSkyImage,
+        "02d": clearSkyImage,
+        "03d": clearSkyImage,
+        "04d": clearSkyImage,
+        "09d": clearSkyImage,
+        "10d": clearSkyImage,
+        "11d": clearSkyImage,
+        "13d": clearSkyImage,
+        "50d": clearSkyImage,
+      };
+      setBackgroundVideo(
+        weatherVideoMap[data.weather[0].icon] || clearSkyImage,
+      );
+
+      const geoResponse = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyAW0-OQUNUuQHQ-TvSuo4v4GjRKmHE1eps`,
+      );
+      const geoData = await geoResponse.json();
+
+      if (geoData.results && geoData.results.length > 0) {
+        const location = geoData.results[0].geometry.location;
+        setCenter({ lat: location.lat, lng: location.lng });
+      } else {
+        console.error("No results found for the specified city.");
       }
-    };
+    } catch (error) {
+      console.error("Failed to fetch weather data:", error);
+    }
+  };
 
-    useEffect(() => {
-      fetchWeatherAndGeocodeData('Villeurbanne');
-    }, []);
+  useEffect(() => {
+    fetchWeatherAndGeocodeData("Villeurbanne");
+  }, []);
 
-    const search = async () => {
-      const inputElement = document.querySelector(".cityInput");
-      const city = inputElement.value;
+  const search = async () => {
+    const inputElement = document.querySelector(".cityInput");
+    const city = inputElement.value;
 
-      /*
+    /*
       if (city === "") {
         alert("Please enter a city name.");
         return;
       }
       */
 
-      await fetchWeatherAndGeocodeData(city);
-    };
+    await fetchWeatherAndGeocodeData(city);
+  };
 
-    return (
-    <div className='weatherapp-container'>
-        <img
-          src={backgroundVideo}
-          alt="Background"
-          style={{
-            position: "absolute",
-            width: "100%",
-            left: "0",
-            top: "0",
-            height: "100%",
-            objectFit: "cover",
-            zIndex: "-1"
-          }}
-          key={backgroundVideo} 
-        />
-       {/* <div className="header">
+  return (
+    <div className="weatherapp-container">
+      <img
+        src={backgroundVideo}
+        alt="Background"
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "0",
+          top: "0",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: "-1",
+        }}
+        key={backgroundVideo}
+      />
+      {/* <div className="header">
             <Header/>
         </div> */}
       <div className="weatherapp-top-bar">
         <input
           type="text"
           className="cityInput"
-          placeholder='Ville'
+          placeholder="Ville"
           onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              search(); 
-              e.preventDefault(); 
+            if (e.key === "Enter") {
+              search();
+              e.preventDefault();
             }
           }}
         />
-
-        
       </div>
       <div className="weatherapp-weather-temp">{weatherData.temp}</div>
       <div className="weatherapp-weather-location">{weatherData.location}</div>
@@ -163,7 +165,7 @@ export const WeatherApp = () => {
         </div>
       </div>
       <div className="weatherapp-map-container">
-        <MapComponent center={center}/>
+        <MapComponent center={center} />
       </div>
     </div>
   );
