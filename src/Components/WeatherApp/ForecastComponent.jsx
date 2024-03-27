7b35386146d83b73e5ff3e8f349ca0f74929222a
alaaeddine <alaaeddine.ahriz@insa-lyon.fr>
2024-03-27 21:51:38 +0100

    prettier + css forecast

--- src/Components/WeatherApp/ForecastComponent.jsx
+++ src/Components/WeatherApp/ForecastComponent.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './WeatherForecastList.css'; // Ensure the CSS file name matches
+import React from "react";
+import "./WeatherForecastList.css"; // Ensure the CSS file name matches
 
 function WeatherForecastList({ forecasts }) {
-
   if (!forecasts || !forecasts.data) {
     // You could return a loading indicator or null here
-    return <div className='Loading'>Loading...</div>;
+    return <div className="Loading">Loading...</div>;
   }
   // Helper function to format date
   const formatDate = (datetime) => {
@@ -14,27 +13,27 @@ function WeatherForecastList({ forecasts }) {
     today.setHours(0, 0, 0, 0); // Reset today's time to 00:00 for accurate comparison
 
     if (date.toDateString() === today.toDateString()) {
-      return 'Today';
+      return "Today";
     } else {
-      return date.toLocaleDateString('en-US', { weekday: 'long' }); // Change 'en-US' as needed
+      return date.toLocaleDateString("en-US", { weekday: "long" }); // Change 'en-US' as needed
     }
   };
 
-  const forecastItems = forecasts.data.map((forecast, index) => (
-    <li key={index} className="forecast-item">
-      <div className="forecast-date-desc">
-        <div className="forecast-date">{formatDate(forecast.datetime)}</div>
-        <div className="forecast-description">{forecast.weather.description}</div>
-      </div>
-      <div className="forecast-temps">
-        <span className="temp-min">{forecast.low_temp}°C</span>
-        <div className="temp-bar" style={{
-          backgroundImage: `linear-gradient(to right, #add8e6, #ffa07a)`
-        }}></div>
-        <span className="temp-max">{forecast.high_temp}°C</span>
-      </div>
-    </li>
-  )) || 'Loading...';
+  const forecastItems =
+    forecasts.data.map((forecast, index) => (
+      <li key={index} className="forecast-item">
+        <div className="forecast-date-desc">
+          <div className="forecast-date">{formatDate(forecast.datetime)}</div>
+          <div className="forecast-description">
+            {forecast.weather.description}
+          </div>
+        </div>
+        <div className="forecast-temps">
+          <span className="temp-min">{forecast.low_temp}°</span>
+          <span className="temp-max">{forecast.high_temp}°C</span>
+        </div>
+      </li>
+    )) || "Loading...";
 
   return (
     <div className="forecast-container">
