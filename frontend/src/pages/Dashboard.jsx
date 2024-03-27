f30f386e33ae89c3c44b858b00a1b811aec57911
Nicolas Lepotier <nicolaslepotier3@gmail.com>
2024-03-27 18:18:07 +0100

    CSSv1

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -30,30 +30,41 @@ function Dashboard() {
 
     return ( 
         <>
-        <div>
-            <h2>dashboard</h2>
-            <p>{suc}</p>
-        </div>
-        
-        <div className='dashboard'>
+            <div>
+                <h2>Dashboard</h2>
+                <p>{suc}</p>
+            </div>
+            
+            <div className='dashboard'>
                 <div className='profil'>
                     <div className='photo'>
+                        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
                     </div>
-                    <div className='description'>
-                    <p>{info["nom"] + " " + info["Prenom"]}</p>
-                    <p>{info["user"]}</p>
+                    <div className='info-widget'>
+                        <p>{info["nom"] + " " + info["Prenom"]}</p>
+                    </div>
+                    <div className='info-widget'>
+                        <p>{info["user"]}</p>
                     </div>
                     <div className='preferences'>
-                    <p>{info["Ville_par_défaut"]}</p>
+                        <p>Préférences</p>
+                        <div className='info-widget'>
+                            <p>{info["Ville_par_défaut"]}</p>
+                        </div>
+                        <p>Options</p>
                         <div className='option'>
-                            option
+                            <input type="checkbox" id="option1" name="option1" value="option1" />
+                            <label htmlFor="option1">Pluie</label><br />
+                            <input type="checkbox" id="option2" name="option2" value="option2" />
+                            <label htmlFor="option2">Vent</label><br />
+                            <input type="checkbox" id="option3" name="option3" value="option3" />
+                            <label htmlFor="option3">Humidité</label><br />
                         </div>
                     </div>
                 </div>
-        </div>
-            </>
-
-     );
+            </div>
+        </>
+    );
 }
 
 export default Dashboard;
