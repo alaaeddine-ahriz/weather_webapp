621d46c3ba4ec19ff594b01a737f101773b95b9d
alaaeddine <alaaeddine.ahriz@insa-lyon.fr>
2024-03-27 01:31:46 +0100

    css login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios'
-import './Login.css'
-import { Header } from "../WeatherApp/HeaderComponent"
-
-
+import axios from "axios";
+import "./Login.css";
+import { Header } from "../WeatherApp/HeaderComponent";
 
 function Login() {
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const navigate = useNavigate()
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
 
-    axios.defaults.withCredentials = true;
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:5200/login', {email, password})
-        .then(res => {
-            console.log("login: " + res.data);
-            if(res.data.Status === "Success") {
-                if(res.data.role === "admin") {
-                    navigate('/dashboard')
-                } else {
-                    navigate('/home')
-                }
-            }
-        }).catch(err => console.log(err))
-    }
+  axios.defaults.withCredentials = true;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5200/login", { email, password })
+      .then((res) => {
+        console.log("login: " + res.data);
+        if (res.data.Status === "Success") {
+          if (res.data.role === "admin") {
+            navigate("/dashboard");
+          } else {
+            navigate("/home");
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
-    return(
-      <div className='login-container'>
-        {/* <div className="login-header">
+  return (
+    <div className="login-container">
+      {/* <div className="login-header">
             <Header/>
         </div> */}
-      <div className='login-form'>
-        <h2 className="login-seconnecter-titre">
-          Ravis de vous revoir
-        </h2>
+      <div className="login-form">
+        {/* <h2 className="login-seconnecter-titre">toz de vous revoir</h2> */}
         <form onSubmit={handleSubmit}>
           <div className="login-element">
             {/* <label htmlFor="email">
@@ -63,18 +61,15 @@ function Login() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit">
-            Se connecter
-          </button>
-          </form>
-          <p>Already Have an Account</p>
-          <Link to="/">
-            Sign Up
-          </Link>
-        
+          <button type="submit">Se connecter</button>
+        </form>
+        {/* <p>Already Have an Account</p> */}
+        <div className="back">
+          <Link to="/">Retour à la météo</Link>
+        </div>
       </div>
     </div>
-    )
+  );
 }
 
 export default Login;
