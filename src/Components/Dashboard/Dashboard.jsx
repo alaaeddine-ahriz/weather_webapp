660cc8473e72feff8693db8fd07ad652a94277a6
Ounissi <mikailoun.123@hotmail.fr>
2024-03-28 15:51:35 +0100

    Merge de la branche dash, rajout du css dashboard et jsx

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Dashboard.css"
 import axios from 'axios'
 
 function Dashboard() {
     const [suc, setSuc ] = useState()
+    const [info, setInfo] = useState([]);
     const navigate = useNavigate()
     axios.defaults.withCredentials = true;
     useEffect(()=> {
@@ -17,13 +19,52 @@ function Dashboard() {
             }
         }).catch(err => console.log(err))
     }, [])
-    return ( 
-        <div>
-            <h2>dashboard</h2>
-            <p>{suc}</p>
-        </div>
+    useEffect(()=> {
+        axios.get('http://localhost:4000/get-nom/0769611797')
+        .then(res => {
+            console.log("dashboad: " + res.data["nom"]);
+            setInfo(res.data);
+        }).catch(err => console.log(err))
+    }, [])
 
-     );
+
+    return ( 
+        <>
+            <div>
+                <h2>Dashboard</h2>
+                <p>{suc}</p>
+            </div>
+            
+            <div className='dashboard'>
+                <div className='profil'>
+                    <div className='photo'>
+                        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
+                    </div>
+                    <div className='info-widget'>
+                        <p>{info["nom"] + " " + info["Prenom"]}</p>
+                    </div>
+                    <div className='info-widget'>
+                        <p>{info["user"]}</p>
+                    </div>
+                    <div className='preferences'>
+                        <p>Préférences</p>
+                        <div className='info-widget'>
+                            <p>{info["Ville_par_défaut"]}</p>
+                        </div>
+                        <p>Options</p>
+                        <div className='option'>
+                            <input type="checkbox" id="option1" name="option1" value="option1" />
+                            <label htmlFor="option1">Pluie</label><br />
+                            <input type="checkbox" id="option2" name="option2" value="option2" />
+                            <label htmlFor="option2">Vent</label><br />
+                            <input type="checkbox" id="option3" name="option3" value="option3" />
+                            <label htmlFor="option3">Humidité</label><br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Dashboard;
